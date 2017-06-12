034108f286f0f49d08723fe347cc0eb7bf92b2ad
Kim Seonghyun <shyeon.kim@scipi.net>
2017-06-12 22:42:33 +0200

    Add trope_cache
    Signed-off-by: Kim Seonghyun <shyeon.kim@scipi.net>

--- R/data.R
+++ R/data.R
@@ -1,4 +1,72 @@
+change_redirects <- function(links,
+                             redirect_to_cache_dir = tempdir(),
+                             filter_pattern = "/Main/",
+                             verbose = F) {
+  stopifnot(dir.exists(redirect_to_cache_dir))
+
+  ret <- c()
+  for (i in 1:length(links)) {
+    trope_url <- links[i]
+    key <- digest::digest(trope_url)
+    file_path <- file.path(redirect_to_cache_dir,
+                           paste0(key, "_redirect_to.csv"))
+    if (!file.exists(file_path)) {
+      warning(file_path, " is not exist")
+      trope_redirect_to(trope_url, redirect_to_cache_dir)
+      stopifnot(file.exists(file_path))
+    }
+
+    .df <- read.csv2(file_path)
+    if(.df[1, "redirected"] == T) {
+      ret <- c(ret, as.character(.df[1, "redirect_to"]))
+    } else {
+      ret <- c(ret, as.character(trope_url))
+    }
+  }
+
+  stopifnot(length(links) == length(ret))
+  ret
+}
+
+#' Prepare cache for trope data
+#'
+#' 1) redirect_to handling
+#' 2) trope data fetching handling
+#'
+#' @export
+trope_cache <- function(trope_urls,
+                        depth = 1,
+                        trope_cache_dir = tempdir(),
+                        redirect_to_cache_dir = tempdir(),
+                        sleep = .5,
+                        verbose = F) {
+  stopifnot(dir.exists(trope_cache_dir))
+  stopifnot(dir.exists(redirect_cache_dir))
+
+  while(depth > 0) {
+    if (verbose) {
+      cat("################################################################\n")
+      cat("# Depth: ", depth, "............................................\n")
+    }
+    urls <- trope_redirect_to(trope_urls,
+                              cache_dir = redirect_cache_dir,
+                              verbose = verbose)
+
+    ret <- trope_data(urls,
+                      cache_dir = trope_cache_dir,
+                      verbose = verbose)
+
+    trope_urls <- ret$link
+    depth <- depth - 1
+  }
+
+  if (verbose) {
+    cat("Successfully prepared cache")
+  }
+}
+
 #' @importFrom digest digest
+#' @importFrom magrittr %>%
 #' @export
 trope_data <- function(trope_urls,
                        cache_dir = tempdir(),
@@ -12,7 +80,7 @@ trope_data <- function(trope_urls,
 
     if (verbose) {
       cat(toString(Sys.time()),
-          " | Checking[", i, "/", length(trope_urls), "] | ",
+          " | ", i, "/", length(trope_urls), " | ",
           trope_url, "... ", sep = "")
     }
 
@@ -65,6 +133,7 @@ trope_data <- function(trope_urls,
 }
 
 #' @importFrom digest digest
+#' @importFrom magrittr %>%
 #' @export
 trope_redirect_to <- function(trope_urls,
                               cache_dir = tempdir(),
@@ -76,7 +145,7 @@ trope_redirect_to <- function(trope_urls,
 
     if (verbose) {
       cat(toString(Sys.time()),
-          " | Checking[", i, "/", length(trope_urls), "] | ",
+          " | ", i, "/", length(trope_urls), " | ",
           trope_url, "... ", sep = "")
     }
 
@@ -131,5 +200,3 @@ trope_redirect_to <- function(trope_urls,
                      NULL
                  }}))
 }
-
-
